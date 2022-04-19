61526286fab88e4e22de316a8511b7d5660128b1
Brandi Cameron <hello@brandicameron.com>
2022-04-19 15:24:04 -0500

    Added a splash page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import './scss/App.scss';
+import { useState, useEffect } from 'react';
 import { useUser } from './hooks/useUser';
 import LoginPage from './components/LoginPage';
 import Header from './components/Header';
 import Ratings from './components/Ratings';
 import Readings from './components/Readings';
 import NewReading from './components/NewReading';
+import SplashPage from './components/SplashPage';
 
-function App() {
+export default function App() {
+  const [showSplashPage, setShowSplashPage] = useState(true);
   const { loggedIn } = useUser();
+
+  useEffect(() => {
+    let timer1 = setTimeout(() => {
+      setShowSplashPage(false);
+    }, 1500);
+    return () => {
+      clearTimeout(timer1);
+    };
+  }, []);
 
   return (
     <>
+      {showSplashPage && <SplashPage />}
       {!loggedIn && <LoginPage />}
       {loggedIn && (
         <>
@@ -23,5 +36,3 @@ function App() {
     </>
   );
 }
-
-export default App;
---
+++ src/components/SplashPage.js
@@ -0,0 +1,9 @@
+import SplashHeart from '../images/splash-heart.svg';
+
+export default function SplashPage() {
+  return (
+    <div className='splash-page'>
+      <img className='splash-heart' src={SplashHeart} alt='' />
+    </div>
+  );
+}
